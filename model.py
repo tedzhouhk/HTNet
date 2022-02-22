7890133de6e90c2f04bcea38325163c41ec93e2e
hongkuan <442724184@qq.com>
2022-02-22 10:35:32 -0800

    hetero working

--- model.py
+++ model.py
@@ -167,6 +167,71 @@ class EGCNConv(nn.Module):
             h_out = graph.ndata['h_out'].view(-1, self._out_node_feats)
             return h_out, f_out
 
+class EGATConv(nn.Module):
+
+    def __init__(self,
+                 in_node_feats,
+                 in_edge_feats,
+                 out_node_feats,
+                 out_edge_feats,
+                 num_heads,
+                 bias=True):
+
+        super().__init__()
+        self._num_heads = num_heads
+        self._out_node_feats = out_node_feats
+        self._out_edge_feats = out_edge_feats
+        self.fc_node = nn.Linear(in_node_feats, out_node_feats*num_heads, bias=True)
+        self.fc_ni = nn.Linear(in_node_feats, out_edge_feats*num_heads, bias=False)
+        self.fc_fij = nn.Linear(in_edge_feats, out_edge_feats*num_heads, bias=False)
+        self.fc_nj = nn.Linear(in_node_feats, out_edge_feats*num_heads, bias=False)
+        self.attn = nn.Parameter(torch.FloatTensor(size=(1, num_heads, out_edge_feats)))
+        if bias:
+            self.bias = nn.Parameter(torch.FloatTensor(size=(num_heads * out_edge_feats,)))
+        else:
+            self.register_buffer('bias', None)
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        gain = init.calculate_gain('relu')
+        init.xavier_normal_(self.fc_node.weight, gain=gain)
+        init.xavier_normal_(self.fc_ni.weight, gain=gain)
+        init.xavier_normal_(self.fc_fij.weight, gain=gain)
+        init.xavier_normal_(self.fc_nj.weight, gain=gain)
+        init.xavier_normal_(self.attn, gain=gain)
+        init.constant_(self.bias, 0)
+
+    def forward(self, graph, nfeats, efeats, get_attention=False):
+        with graph.local_scope():
+
+            f_ni = self.fc_ni(nfeats)
+            f_nj = self.fc_nj(nfeats)
+            f_fij = self.fc_fij(efeats)
+            graph.srcdata.update({'f_ni': f_ni})
+            graph.dstdata.update({'f_nj': f_nj})
+            # add ni, nj factors
+            graph.apply_edges(dgl.function.u_add_v('f_ni', 'f_nj', 'f_tmp'))
+            # add fij to node factor
+            f_out = graph.edata.pop('f_tmp') + f_fij
+            if self.bias is not None:
+                f_out = f_out + self.bias
+            f_out = nn.functional.leaky_relu(f_out)
+            f_out = f_out.view(-1, self._num_heads, self._out_edge_feats)
+            # compute attention factor
+            e = (f_out * self.attn).sum(dim=-1).unsqueeze(-1)
+            graph.edata['a'] = dglnn.edge_softmax(graph, e)
+            graph.ndata['h_out'] = self.fc_node(nfeats).view(-1, self._num_heads,
+                                                             self._out_node_feats)
+            # calc weighted sum
+            graph.update_all(dgl.function.u_mul_e('h_out', 'a', 'm'),
+                             dgl.function.sum('m', 'h_out'))
+
+            h_out = graph.ndata['h_out'].view(-1, self._num_heads, self._out_node_feats)
+            if get_attention:
+                return h_out, f_out, graph.edata.pop('a')
+            else:
+                return h_out.view(-1, self._out_node_feats), f_out.view(-1, self._out_edge_feats)
+
 class EHeteroGraphConv(nn.Module):
 
     def __init__(self, mods):

1b973698515fbb13cfa8df3552e45d808ddea613
hongkuan <hongkuaz@usc.edu>
2022-03-08 11:17:30 -0800

    add sinr-based method

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ parser.add_argument('--gpu', type=int, default=0, help='which gpu to use')
 parser.add_argument('--data', type=str, help='which dataset to use')
 parser.add_argument('--batch_size', type=int, default=1, help='batch size')
 parser.add_argument('--num_snapshot', type=int, default=10, help='number of snapshot')
+parser.add_argument('--sinr', action='store_true', help='whether to use linear regression based on SINR (this only works on CPU)')
 parser.add_argument('--gbrt', action='store_true', help='whether to use gradient boosted regression tree (this only works on CPU)')
 parser.add_argument('--graph', action='store_true', help='whether to use graph information')
 parser.add_argument('--hetero', action='store_true', help='whether to treat as heterogeneous graph')
@@ -26,8 +27,9 @@ import numpy as np
 from minibatch import get_dataloader
 from model import NetModel
 from datetime import datetime
+from sklearn import linear_model
 
-if not args.gbrt:
+if not args.gbrt and not args.sinr:
     train_dataloader, valid_dataloader, test_dataloader = get_dataloader('data/{}/processed/'.format(args.data), args.batch_size, all_cuda=True)
 
     model = NetModel(args.layer, args.dim, args.graph, args.hetero, args.dynamic, args.num_snapshot).cuda()
@@ -92,7 +94,7 @@ if not args.gbrt:
     print('Loading model in epoch {}...'.format(best_e))
     model.load_state_dict(torch.load(model_fn))
     print('Test RMSE: {:.4f}'.format(eval(model, test_dataloader, output=args.output)))
-else:
+elif args.gbrt:
     # GBRT using xgb library
     train_dataloader, _, test_dataloader = get_dataloader('data/{}/processed/'.format(args.data), args.batch_size, all_cuda=False)
 
@@ -119,6 +121,52 @@ else:
     test_y = np.concatenate(test_y, axis=None)
     
     model = xgboost.XGBRegressor(max_depth=4, n_estimators=100)
+    model.fit(train_x, train_y)
+    pred = torch.from_numpy(model.predict(test_x))
+    true = torch.from_numpy(test_y)
+    rmse = float(torch.sqrt(torch.nn.functional.mse_loss(pred, true) + 1e-8))
+    if args.output != '':
+        if not os.path.exists('output'):
+            os.mkdir('output')
+        output_fn = 'output/{}.pkl'.format(args.output)
+        outs = dict()
+        s_idx = 0
+        e_idx = 0
+        for idx, l in zip(test_idx, test_length):
+            e_idx += l
+            out = {'pred':pred[s_idx:e_idx].numpy(), 'true':true[s_idx:e_idx].numpy()}
+            outs[idx] = out
+            s_idx += l
+        with open(output_fn, 'wb') as f:
+            pickle.dump(outs, f)
+    print('Test RMSE: {:.4f}'.format(rmse))
+elif args.sinr:
+    # linear regression using Scikit-learn
+    train_dataloader, _, test_dataloader = get_dataloader('data/{}/processed/'.format(args.data), args.batch_size, all_cuda=False)
+
+    train_x = list()
+    train_y = list()
+    for g, _ in train_dataloader:
+        mask = (g.nodes['sta'].data['mask'] > 0).squeeze()
+        train_x.append(torch.cat([g.nodes['sta'].data['feat'], g.edges['sta_ap'].data['feat']], dim=1)[mask].numpy())
+        train_y.append(g.nodes['sta'].data['throughput'][mask].numpy())
+    train_x = np.concatenate(train_x, axis=0)[:, 20].reshape(-1, 1)
+    train_y = np.concatenate(train_y, axis=None)
+
+    test_x = list()
+    test_y = list()
+    test_idx = list()
+    test_length = list()
+    for g, idx in test_dataloader:
+        mask = (g.nodes['sta'].data['mask'] > 0).squeeze()
+        test_x.append(torch.cat([g.nodes['sta'].data['feat'], g.edges['sta_ap'].data['feat']], dim=1)[mask].numpy())
+        test_y.append(g.nodes['sta'].data['throughput'][mask].numpy())
+        test_idx.append(int(idx))
+        test_length.append(test_y[-1].shape[0])
+    test_x = np.concatenate(test_x, axis=0)[:, 20].reshape(-1, 1)
+    test_y = np.concatenate(test_y, axis=None)
+    
+    model = linear_model.LinearRegression()
     model.fit(train_x, train_y)
     pred = torch.from_numpy(model.predict(test_x))
     true = torch.from_numpy(test_y)

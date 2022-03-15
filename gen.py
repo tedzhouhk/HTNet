dd0f5272544162ce00120afd442978a25d54052f
hongkuan <442724184@qq.com>
2022-03-15 13:22:39 -0700

    add script for setup5

--- gen.py
+++ gen.py
@@ -278,3 +278,68 @@ if args.setup == 4:
             mfn = 'data/setup4/raw/{}_{}.pkl'.format(i, input_files[p].split('/')[-1][:-4])
             with open(mfn, 'wb') as f:
                 pickle.dump(target_idxs, f)
+
+if args.setup == 5:
+    # setup 4: cross-AP moving sta + dynamic channel allocation (2 + 4)
+    print('generating setup 5...')
+    frac_moving = 0.5
+    max_speed = 1 #m/snapshot
+    min_speed = 0.1 #m/snapshot
+
+    if not os.path.exists('data/setup5'):
+        os.mkdir('data/setup5')
+    if not os.path.exists('data/setup5/raw'):
+        os.mkdir('data/setup5/raw')
+    moved = 0
+    unmoved = 0
+    for p in tqdm(range(len(input_files))):
+        odf = pd.read_csv(input_files[p],sep=';')
+        wlan_codes = list(set(odf['wlan_code'].tolist()))
+        ap_pos = get_ap_pos(odf)
+        for i in range(num_scenarios):
+            df = odf.copy(deep=True)
+            moving_idxs = list()
+            for c in wlan_codes:
+                moving_idxs += list(df[(df['wlan_code'] == c) & (df['node_type'] == 1)].sample(frac=frac_moving).index.values)
+            speeds = np.random.uniform(min_speed, max_speed, len(moving_idxs))
+            rads = np.random.uniform(0, 2 * math.pi, len(moving_idxs))
+            for k in range(num_snapshots):
+                if k == 0:
+                    df['moved'] = 0
+                for j, idx in enumerate(moving_idxs):
+                    c = df.loc[idx]['wlan_code']
+                    x = df.loc[idx]['x(m)'] + speeds[j] * math.cos(rads[j])
+                    y = df.loc[idx]['y(m)'] + speeds[j] * math.sin(rads[j])
+                    r = ((x - ap_pos[c]['x']) ** 2 + (y - ap_pos[c]['y']) ** 2) ** 0.5
+                    if r < ap_pos[c]['r']:
+                        # move within AP
+                        df.at[idx, 'x(m)'] = x
+                        df.at[idx, 'y(m)'] = y
+                        moved += 1
+                    else:
+                        new_ap_c = check_ap_range(x, y, ap_pos)
+                        if new_ap_c != -1:
+                            # move across AP
+                            df.at[idx, 'x(m)'] = x
+                            df.at[idx, 'y(m)'] = y
+                            df.at[idx, 'wlan_code'] = new_ap_c
+                            moved += 1
+                        else:
+                            # exceed AP ranges, not moving
+                            unmoved += 1
+                    if k ==0:
+                        df.at[idx, 'moved'] = 1
+                fn = 'data/setup5/raw/{}_{}_{}'.format(i, k, input_files[p].split('/')[-1])
+                sorted_df = df.copy(deep=True)
+                sorted_df.sort_values(by=['wlan_code', 'node_type'], inplace=True, ignore_index=True)
+                if k == 0:
+                    first_snapshot_moving_idxs = list(sorted_df[sorted_df['moved'] == 1].index)
+                sorted_df.drop('moved', axis=1, inplace=True)
+                allocate_dynamic_channel(sorted_df)
+                sorted_df.to_csv(fn, index=False, sep=';')
+                adf = sorted_df
+                bdf = pd.read_csv(fn, sep=';')
+            mfn = 'data/setup5/raw/{}_{}.pkl'.format(i, input_files[p].split('/')[-1][:-4])
+            with open(mfn, 'wb') as f:
+                pickle.dump(first_snapshot_moving_idxs, f)
+    print('moved: {}%'.format(moved / (moved + unmoved) * 100))
